bd9294da8ea838b80ee357eece9b6ed9c6b1ef6a
mdavidstefan <marton.david.stefan00@gmail.com>
2024-03-22 10:04:00 +0100

    xdd

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ console.log(randNr);
 
 let result = compare(99, randNr);
 console.log(result);
+
+document.querySelector(".checkBtn").addEventListener("click", check);
+
+let tries = 5;  
+document.getElementById("life").textContent = "Próbálkozási lehetőségek száma: " + tries
+
+function check() {
+  if (tries > 0) {
+    let guess = document.getElementById("inputArea").value;
+    document.getElementById("resultgi").textContent = compare(guess, randNr);
+    tries--;
+    document.getElementById("life").textContent == "Próbálkozási lehetőségek száma: " + tries
+    return;
+  }
+  document.getElementById("life").textContent ==
+    "Nincs több próbálkozási lehetőség!";
+  document.querySelector(".checkBtn").classList.toggle("disabled");
+}
