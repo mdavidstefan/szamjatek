962a59919c8e2848300448de23b6d08b145b78a5
mdavidstefan <marton.david.stefan00@gmail.com>
2024-03-22 13:26:23 +0100

    logicdone

--- compare.js
+++ compare.js
@@ -1,9 +1,9 @@
 export function compare(userNumber, randNumber) {
-	if (userNumber < randNumber) {
-		return 'A te számod kisebb...';
-	} else if (userNumber > randNumber) {
-		return 'A te számod nagyobb';
-	} else {
-		return 'Helyes megfejtés AZ OTT!4!!44!4!';
-	}
+  if (userNumber < randNumber) {
+    return "A te számod kisebb...";
+  } else if (userNumber > randNumber) {
+    return "A te számod nagyobb!";
+  } else if (userNumber == randNumber) {
+    return "Helyes megfejtés AZ OTT!4!!44!4!";
+  }
 }
--- main.js
+++ main.js
@@ -5,22 +5,23 @@ let randNr = genRandNumber(1, 100);
 console.log(randNr);
 
 let result = compare(99, randNr);
-console.log(result);
 
 document.querySelector(".checkBtn").addEventListener("click", check);
 
-let tries = 5;  
-document.getElementById("life").textContent = "Próbálkozási lehetőségek száma: " + tries
+let tries = 5;
+document.getElementById("life").textContent =
+  "Próbálkozási lehetőségek száma: " + tries;
 
 function check() {
-  if (tries > 0) {
-    let guess = document.getElementById("inputArea").value;
-    document.getElementById("resultgi").textContent = compare(guess, randNr);
+  if (tries > 1) {
+    let guess = parseInt(document.getElementById("inputArea").value);
+    document.getElementById("result").textContent = compare(guess, randNr);
     tries--;
-    document.getElementById("life").textContent == "Próbálkozási lehetőségek száma: " + tries
+    document.getElementById("life").textContent =
+      "Próbálkozási lehetőségek száma: " + tries;
     return;
   }
-  document.getElementById("life").textContent ==
+  document.getElementById("life").textContent =
     "Nincs több próbálkozási lehetőség!";
-  document.querySelector(".checkBtn").classList.toggle("disabled");
+  document.querySelector(".checkBtn").disabled = true;
 }
