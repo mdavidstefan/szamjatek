6e5f20e7afa2b6c11fd49a89297a2e21c262bc2b
mdavidstefan <marton.david.stefan00@gmail.com>
2024-03-22 13:59:30 +0100

    final

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ import { genRandNumber } from "./getRandNumber.js";
 import { compare } from "./compare.js";
 
 let randNr = genRandNumber(1, 100);
-console.log(randNr);
+console.log("A kitalálandó szám: " + randNr);
 
 let result = compare(99, randNr);
 
 document.querySelector(".checkBtn").addEventListener("click", check);
 
 let tries = 5;
-document.getElementById("life").textContent =
+document.getElementById("guesses").textContent =
   "Próbálkozási lehetőségek száma: " + tries;
 
 function check() {
@@ -17,11 +17,12 @@ function check() {
     let guess = parseInt(document.getElementById("inputArea").value);
     document.getElementById("result").textContent = compare(guess, randNr);
     tries--;
-    document.getElementById("life").textContent =
+    document.getElementById("guesses").textContent =
       "Próbálkozási lehetőségek száma: " + tries;
     return;
   }
-  document.getElementById("life").textContent =
+
+  document.getElementById("guesses").textContent =
     "Nincs több próbálkozási lehetőség!";
   document.querySelector(".checkBtn").disabled = true;
 }
