7f548c8c5faaf3ae64c98f5d31a2a8134fd81843
korneXD <korcika2005@gmail.com>
2024-03-22 09:22:29 +0100

    ősbagoly

--- compare.js
+++ compare.js
@@ -4,6 +4,6 @@ export function compare(userNumber, randNumber) {
 	} else if (userNumber > randNumber) {
 		return 'A te számod nagyobb';
 	} else {
-		return 'Helyes megfejtés!';
+		return 'Helyes megfejtés AZ OTT!4!!44!4!';
 	}
 }
